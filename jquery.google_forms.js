7237dcb2696e78b5c5074db397daeaf689b93020
Tom Fennelly <tom.fennelly@gmail.com>
2013-03-15 07:46:56 +0000

    Changed to only add iframe on form submit

--- jquery.google_forms.js
+++ jquery.google_forms.js
@@ -5,26 +5,29 @@
         return this.each(function () {
             try {
                 var theForm = $(this);
-                var targetFrameName = 'hidden_google_form_iframe_' + formCounter;
 
-                theForm.attr('target', targetFrameName);
+                theForm.submit(function() {
+                    var targetFrameName = 'hidden_google_form_iframe_' + formCounter;
 
-                var iFrame = $('<iframe>', {name: targetFrameName}).css({display: 'none'});
-                iFrame.insertBefore(theForm);
+                    theForm.attr('target', targetFrameName);
 
-                iFrame.load(function() {
-                    if (typeof onSubmit === 'string') {
-                        window.location.replace(onSubmit);
-                    } else if (typeof onSubmit === 'function') {
-                        onSubmit();
-                    } else if (typeof onSubmit === 'object') {
-                        if (onSubmit.onsubmit) {
-                            onSubmit.onsubmit();
+                    var iFrame = $('<iframe>', {name: targetFrameName}).css({display: 'none'});
+                    iFrame.insertBefore(theForm);
+
+                    iFrame.load(function() {
+                        if (typeof onSubmit === 'string') {
+                            window.location.replace(onSubmit);
+                        } else if (typeof onSubmit === 'function') {
+                            onSubmit();
+                        } else if (typeof onSubmit === 'object') {
+                            if (onSubmit.onsubmit) {
+                                onSubmit.onsubmit();
+                            }
+                            if (onSubmit.redirect) {
+                                window.location.replace(onSubmit.redirect);
+                            }
                         }
-                        if (onSubmit.redirect) {
-                            window.location.replace(onSubmit.redirect);
-                        }
-                    }
+                    });
                 });
             } finally {
                 formCounter++;
